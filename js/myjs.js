f34d2635e60c4abb684d749fdd15c84b59fb035f
licnlt <1079682183@qq.com>
2016-11-08 19:36:03 +0800

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,58 @@
+$(".headLine").hover(function(){
+	$(this).text("Web Developer /  Resume");
+},function(){
+	$(this).text("前端工程师  /  个人简历");
+});
+
+
+$("#fullpageMenu").find("a").hover(function(){
+	$(this).css("border-bottom","2px solid #0055ff");
+},function(){
+	$(this).css("border-bottom","");
+});
+
+//第一屏动画
+function section1(){
+	$(".Text").slideDown(1000,function(){
+		$(".my_name").slideDown(400,function(){
+			$(".my_job").slideDown(400,function(){
+				$(".Goal").slideDown(400,function(){
+					$(".my_email").slideDown(400);
+				});
+			});
+		});
+	});
+};
+// 第二屏动画
+function section2(){
+	$('.conTent').show(function(){
+		$('.conTent').animate({width:"600px",height:"180px"},1000,function(){
+			move('.conTent .Img1').rotate(360).end();
+			move('.conTent .Img2').rotate(360).end();
+			move('.conTent .Img3').rotate(360).end();
+			move('.conTent .Img4').rotate(360).end();
+			$('.con_ico div').slideDown()
+		});
+		$(".conText").fadeIn(1000);
+	});
+};
+//第三屏动画
+function section3(){
+	$(".section_3 img").animate({width:"90px",height:"90px"},1000,function(){
+		$(".section_3 ul").slideDown(1000);
+	});
+	$(".section_3").find("img").on("click",function(){
+		$(this).next("ul").slideToggle(300);
+	})
+};
+// 第四屏动画
+function section4(){
+	$(".Exp>li").not(".myP").on("click",function(event){
+		var index=$(this).index();	
+		$(".Exp>li>p").on("click",function(event){
+		  	return false;
+		});
+		$(".Exp>li>p").eq(index).slideToggle(300);
+		$(".Exp>li>span").eq(index).toggleClass("xuan");
+	});
+};
